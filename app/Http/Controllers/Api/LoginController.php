9e1a5c9203ee873cd77ee29fc9dca15f4e211bd1
Andres Felipe Ordoñez <afoz8424@gmail.com>
2025-01-03 10:04:21 -0500

    Inicio de sesión y generador de tokens

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json(['message' => 'The provided credentials are incorrect.'], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $token = $user->createToken($request->device_name)->plainTextToken;
+
+        return response()->json([
+            'data' => [
+                'attributes' => [
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ],
+                'token' => $token,
+            ]
+        ], Response::HTTP_OK);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,12 +6,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\RecipeController;
 use App\Http\Controllers\Api\TagController;
+use App\Http\Controllers\Api\LoginController;
 
 /* Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum'); */
 
-Route::apiResource('categories', CategoryController::class);
-//Route::get('categories/{id}', [CategoryController::class, 'show']);
-Route::apiResource('recipes', RecipeController::class);
-Route::apiResource('tags', TagController::class);
+Route::post('login', [LoginController::class, 'store']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('recipes', RecipeController::class);
+    Route::apiResource('tags', TagController::class);
+});
