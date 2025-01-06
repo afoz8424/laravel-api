7e9b8127388767e7345210e580faed9776a66829
Andres Felipe Ordoñez <afoz8424@gmail.com>
2025-01-06 14:45:51 -0300

    Inicios de api testing

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+use App\Models\Category;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+
+class CategoryTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_index(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $categories = Category::factory(2)->create();
+        //$response = $this->getJson(route('categories.index'));
+        $response = $this->getJson('api/categories');
+        $response->assertJsonCount(2, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id', 
+                        'type', 
+                        'attributes' => ['name']
+                    ]
+                ]
+            ]
+        );
+
+    }
+
+    public function test_show(): void
+    {
+        Sanctum::actingAs(User::factory()->create());
+
+        $category = Category::factory()->create();
+        //$response = $this->getJson(route('categories.index'));
+        $response = $this->getJson('api/categories/'.$category->id);
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'data' => [
+                    'id', 
+                    'type', 
+                    'attributes' => ['name']
+                ]
+            ]
+        );
+    }
+}
