5d3482df37d656f372bfe85a4dac1c0f42569f54
Andres Felipe Ordoñez <afoz8424@gmail.com>
2025-01-06 14:47:42 -0300

    Ajuste

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -18,7 +18,6 @@ class CategoryTest extends TestCase
         Sanctum::actingAs(User::factory()->create());
 
         $categories = Category::factory(2)->create();
-        //$response = $this->getJson(route('categories.index'));
         $response = $this->getJson('api/categories');
         $response->assertJsonCount(2, 'data')
             ->assertJsonStructure([
@@ -31,7 +30,6 @@ class CategoryTest extends TestCase
                 ]
             ]
         );
-
     }
 
     public function test_show(): void
@@ -39,7 +37,6 @@ class CategoryTest extends TestCase
         Sanctum::actingAs(User::factory()->create());
 
         $category = Category::factory()->create();
-        //$response = $this->getJson(route('categories.index'));
         $response = $this->getJson('api/categories/'.$category->id);
         $response->assertStatus(Response::HTTP_OK)
             ->assertJsonStructure([
